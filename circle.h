5d3662ef6104edf08095e7f4e5ccc15862d26a63
githubgirish2772 <55174702+githubgirish2772@users.noreply.github.com>
2025-07-17 13:28:41 +0530

    Rajesh
    implementation of circle

---
+++ circle.h
@@ -0,0 +1,14 @@
+float circle_area(float r);
+{
+	return 3.14 * r * r;
+}
+
+float circle_diameter(float r);
+{
+	return 2 * r;
+}
+float circle_circumference(float r);
+{
+	return 2 * 3.14 * r 
+}  
+
